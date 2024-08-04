8c5b57f36f1014bc42f0caa1ae46eedd879326d5
Vijay <vijay07appu@gmail.com>
2024-08-04 20:37:25 +0530

    change

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,13 @@ const app = express()
 
 const port = 5000
 
-app.use(cors())
+app.use(cors(
+    {
+        origin:["https://deploy-vijay-project.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+))
 
 
 app.use(express.json())
